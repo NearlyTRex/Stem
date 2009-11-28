6cb5425948300121a86311d24b2e277e64891ba5
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2009-11-28 23:32:02 +0000

    Linux implementation of StubShell
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@106 d291a34c-4c4d-0410-8556-a73de5c6a500

--- stubshell/trunk/source/StubShell.c
+++ stubshell/trunk/source/StubShell.c
@@ -3,7 +3,11 @@
 #if defined(__APPLE__)
 #include <mach/mach.h>
 #include <mach/mach_time.h>
+#elif defined(__linux)
+#include <time.h>
 #endif
+
+#include <stdio.h>
 
 void Shell_mainLoop() {
 }
@@ -19,12 +23,16 @@ double Shell_getCurrentTime() {
 		mach_timebase_info(&timebaseInfo);
 	}
 	return mach_absolute_time() * timebaseInfo.numer / timebaseInfo.denom / 1000000000.0;
+	
 #elif defined(WIN32)
 	//QueryPerformanceCounter
 	return 0;
+	
 #elif defined(__linux)
-	//clock_gettime(CLOCK_MONOTONIC)
-	return 0;
+	struct timespec currentTime;
+	
+	clock_gettime(CLOCK_MONOTONIC, &currentTime);
+	return currentTime.tv_sec + currentTime.tv_nsec * 0.000000001;
 #else
 #error Unimplemented target OS
 #endif
