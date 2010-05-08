6b4bcebb2347a8c0a68bea0d3a9675c51e99e806
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2010-05-08 03:20:28 +0000

    g_openGLAPIVersion (glgraphics/GLInfo.h) is now set to GL_API_VERSION_DESKTOP_1 at startup Refreshed Makefile from template_stemlib Stable_2010-05-06
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@386 d291a34c-4c4d-0410-8556-a73de5c6a500

--- glutshell/trunk/source/glutshell/GLUTShell.c
+++ glutshell/trunk/source/glutshell/GLUTShell.c
@@ -1,3 +1,25 @@
+/*
+  Copyright (c) 2010 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
 #include "shell/Shell.h"
 
 #include <stdio.h>
@@ -21,6 +43,7 @@
 #include <windows.h>
 #endif
 
+#include "glgraphics/GLInfo.h"
 #include "shell/ShellBatteryInfo.h"
 #include "shell/ShellKeyCodes.h"
 #include "shell/Target.h"
@@ -419,6 +442,8 @@ int main(int argc, char ** argv) {
 #ifdef __APPLE__
 	CGLSetParameter(CGLGetCurrentContext(), kCGLCPSwapInterval, &VBL);
 #endif
+	
+	g_openGLAPIVersion = GL_API_VERSION_DESKTOP_1;
 	
 	Target_init(argc, argv);
 	
