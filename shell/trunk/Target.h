5e5292576a8a79a00fc42fa19efc65907e647ede
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2010-05-18 22:34:36 +0000

    Added copyright notices Removed Target_getName() Removed argc and argv from Target_init() Added multi-touch behavior explanation to Target_mouseDragged() documentation
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@423 d291a34c-4c4d-0410-8556-a73de5c6a500

--- shell/trunk/Target.h
+++ shell/trunk/Target.h
@@ -1,13 +1,31 @@
+/*
+  Copyright (c) 2010 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
 #ifndef __TARGET_H__
 #define __TARGET_H__
 
-/** Called by the shell to retrieve a human-readable display name for the target application. */
-const char * Target_getName();
-
 /** Main entry point. Called when the shell's bootstrapping has finished. You should call
-    Shell_mainLoop from this function if you want to continue execution. argc and argv are passed
-    as they were received by main(). */
-void Target_init(int argc, char ** argv);
+    Shell_mainLoop from this function if you want to continue execution. */
+void Target_init();
 
 /** Called by the shell when a frame is ready to be drawn. In OpenGL-based shells, the drawing context
     will have been made current, and you should issue OpenGL calls to draw your scene as appropriate.
@@ -44,7 +62,9 @@ void Target_mouseMoved(float x, float y);
 
 /** Called when the user has moved their mouse cursor, or finger in the case of a touchscreen device.
     For each button that's down, the bit at 1 << buttonNumber (as specified to Target_mouseDown) in
-    buttonMask will be set. See Target_mouseDown for other parameter descriptions. */
+    buttonMask will be set. In the case of multi-touch, when multiple fingers are being moved across
+    the screen, you'll get one call per finger moved with a single bit in buttonMask set each time.
+    See Target_mouseDown for other parameter descriptions. */
 void Target_mouseDragged(unsigned int buttonMask, float x, float y);
 
 #endif
