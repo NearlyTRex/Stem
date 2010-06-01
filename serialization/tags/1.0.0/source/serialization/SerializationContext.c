4c1554146e9709fdf9baee6b6bb26a5973dd24aa
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2010-06-01 01:03:26 +0000

    Tag serialization 1.0.0 source
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@471 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ serialization/tags/1.0.0/source/serialization/SerializationContext.c
@@ -0,0 +1,37 @@
+#include "serialization/SerializationContext.h"
+#include <stdlib.h>
+
+void SerializationContext_init(SerializationContext * self) {
+	StemObject_init((StemObject *) self);
+	
+	self->jmpBuf = NULL;
+	self->status = SERIALIZATION_ERROR_OK;
+	self->dispose = SerializationContext_dispose;
+	self->beginStructure = NULL;
+	self->beginDictionary = NULL;
+	self->beginArray = NULL;
+	self->endStructure = NULL;
+	self->endDictionary = NULL;
+	self->endArray = NULL;
+	self->writeInt8 = NULL;
+	self->writeUInt8 = NULL;
+	self->writeInt16 = NULL;
+	self->writeUInt16 = NULL;
+	self->writeInt32 = NULL;
+	self->writeUInt32 = NULL;
+	self->writeInt64 = NULL;
+	self->writeUInt64 = NULL;
+	self->writeFloat = NULL;
+	self->writeDouble = NULL;
+	self->writeString = NULL;
+	self->writeBoolean = NULL;
+	self->writeEnumeration = NULL;
+	self->writeBitfield8 = NULL;
+	self->writeBitfield16 = NULL;
+	self->writeBitfield32 = NULL;
+	self->writeBitfield64 = NULL;
+}
+
+void SerializationContext_dispose(void * selfPtr) {
+	StemObject_dispose(selfPtr);
+}
