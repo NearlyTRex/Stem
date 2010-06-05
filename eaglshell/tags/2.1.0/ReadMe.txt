6818075d5affd3f6debcaa3ea16539cb8bb615f7
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2010-06-05 00:51:51 +0000

    Tag eaglshell 2.1.0 source
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@512 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ eaglshell/tags/2.1.0/ReadMe.txt
@@ -0,0 +1,33 @@
+EAGLShell is an implementation of a stem shell using EAGL, that builds for iPhone Simulator and iPhone OS (iPhone and iPad). It is a feature complete, production-ready shell.
+
+EAGLShell includes some extensions to the standard shell interface. You should #include "eaglshell/EAGLShell.h" to get the extended interface. It provides facilities for working with the iPhone's status bar, keyboard, and accelerometer. See the documentation in EAGLShell.h for details. Some things to note:
+
+ - In order to receive keyboard events, you must call EAGLShell_showKeyboard() to bring up the onscreen keyboard. Due to API limitations imposed by Apple, key codes passed to Target_keyUp() are not generally trustworthy for any keyboard other than US English, though character codes are reliable. Target_keyDown() is never called.
+
+ - EAGLShell_setOrientation() does not transform your OpenGL coordinate space. It is your responsibility to do so if necessary.
+
+ - You must call EAGLShell_setBatteryMonitoringEnabled(true) if you want to get anything useful from Shell_getBatteryState() and Shell_getBatteryLevel().
+ 
+ - Shell_setFullScreen() toggles the visibility of the application status bar.
+
+ - The application's main loop is already executing when Target_init() is called. For consistency's sake, if you do not call Shell_mainLoop() from within Target_init(), exit() is called and your application will terminate.
+
+ - You must call EAGLShell_setAccelerometerInterval() with a nonzero interval to receive accelerometer events. See the target extension notes below for more details.
+
+
+
+The target interface has also been extended, with EAGLTarget.h defining the new functions that targets must implement. Details on each function are in EAGLTarget.h. There are some differences in behavior for the standard Target functions, as well:
+
+ - Touch events are processed as mouse events. Each finger generates a Target_mouseDown() with a unique ID buttonNumber for multiple fingers at a time. Target_mouseDragged() is called once per finger that moved, with exactly one bit in buttonMask set each time. Target_mouseUp() is called for each finger lifted. Target_mouseMoved() is never called.
+
+ - Touch events can be cancelled. This means it's possible that you'll get calls to Target_mouseDown() and Target_mouseDragged() without matching calls to Target_mouseUp(). To detect and process cancelled touch events, you must implement EAGLTarget_touchesCancelled().
+
+ - Accelerometer events are delivered via EAGLTarget_accelerometer, at the interval specified to EAGLShell_setAccelerometerInterval().
+
+
+
+Other things to be aware of:
+ 
+ - The non-iPad version of EAGLShell compiles against the 3.1 iPhone SDK. This is due only to bugs and instability in iPhone OS 3.0; it doesn't actually use any OS 3.1 features (except CADisplayLink, which is linked in at runtime if available; if not, a timer is used instead), so if desired, you can safely build against the 3.0 SDK.
+
+ - Due to an Apple bug, as of iPhone OS 3.1.3, Target_resized() will not be called when the status bar changes size due to a phone call or voice recording starting or ending. See http://openradar.appspot.com/6475681 for details.
---
+++ eaglshell/tags/2.1.0/source/eaglshell/EAGLShellApplication.m
@@ -0,0 +1,314 @@
+/*
+  Copyright (c) 2010 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#import "eaglshell/EAGLShellApplication.h"
+#import "eaglshell/EAGLView.h"
+#include "shell/ShellKeyCodes.h"
+#include "shell/Target.h"
+#include "eaglshell/EAGLTarget.h"
+
+int g_argc = 0;
+char ** g_argv = NULL;
+
+extern bool mainLoopCalled;
+
+@implementation EAGLShellApplication
+
+- (BOOL) application: (UIApplication *) application didFinishLaunchingWithOptions: (NSDictionary *) dictionary {
+	[self setStatusBarOrientation: UIInterfaceOrientationPortrait animated: NO];
+#if defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && defined(__IPHONE_3_2) && __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_3_2
+	[self setStatusBarHidden: YES withAnimation: UIStatusBarAnimationNone];
+#else
+	[self setStatusBarHidden: YES animated: NO];
+#endif
+	self.applicationSupportsShakeToEdit = NO;
+	
+	if ([dictionary objectForKey: UIApplicationLaunchOptionsURLKey] != nil) {
+		EAGLTarget_openURL([[[dictionary objectForKey: UIApplicationLaunchOptionsURLKey] absoluteString] UTF8String]);
+	}
+	
+	window = [[UIWindow alloc] initWithFrame: [UIScreen mainScreen].bounds];
+	view = [[EAGLView alloc] initWithFrame: [UIScreen mainScreen].applicationFrame];
+	
+	if (view == nil) {
+		exit(EXIT_FAILURE);
+	}
+	
+	textField = [[UITextField alloc] initWithFrame: CGRectMake(0, 0, 0, 0)];
+	textField.text = @" ";
+	textField.delegate = self;
+	[view addSubview: textField];
+	[window addSubview: view];
+	[window makeKeyAndVisible];
+	
+	Target_resized([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+	Target_init();
+	
+	if (!mainLoopCalled) {
+		exit(EXIT_SUCCESS);
+	}
+	
+	[view startAnimation];
+	
+	return YES;
+}
+
+- (void) applicationWillResignActive: (UIApplication *) application {
+}
+
+- (void) applicationDidBecomeActive: (UIApplication *) application {
+}
+
+- (void) applicationWillTerminate: (UIApplication *) application {
+}
+
+- (void) application: (UIApplication *) application didChangeStatusBarOrientation: (UIInterfaceOrientation) oldStatusBarOrientation {
+	[self updateViewFrame];
+}
+
+- (void) application: (UIApplication *) application didChangeStatusBarFrame: (CGRect) oldStatusBarFrame {
+	[self updateViewFrame];
+}
+
+- (void) dealloc {
+	[textField release];
+	[view release];
+	[window release];
+	[super dealloc];
+}
+
+- (enum EAGLShellOpenGLVersion) chosenOpenGLVersion {
+	return [view chosenOpenGLVersion];
+}
+
+- (void) redisplayPosted {
+	[view redisplayPosted];
+}
+
+- (void) showKeyboard {
+	[textField becomeFirstResponder];
+}
+
+- (void) hideKeyboard {
+	[textField resignFirstResponder];
+}
+
+- (BOOL) isKeyboardVisible {
+	return [textField isFirstResponder];
+}
+
+- (void) updateViewFrame {
+	view.frame = [UIScreen mainScreen].applicationFrame;
+}
+
+- (void) enableAccelerometerWithInterval: (NSTimeInterval) interval {
+	[UIAccelerometer sharedAccelerometer].updateInterval = interval;
+	[UIAccelerometer sharedAccelerometer].delegate = self;
+}
+
+- (void) disableAccelerometer {
+	[UIAccelerometer sharedAccelerometer].delegate = nil;
+}
+
+- (void) accelerometer: (UIAccelerometer *) accelerometer didAccelerate: (UIAcceleration *) acceleration {
+	EAGLTarget_accelerometer(acceleration.x, acceleration.y, acceleration.z);
+}
+
+static unsigned int unicharToShellKeyCode(unichar charCode) {
+	switch (charCode) {
+		case '\x1B':
+			return KEYBOARD_ESCAPE;
+		case '`':
+		case '~':
+			return KEYBOARD_GRAVE_ACCENT_AND_TILDE;
+		case '1':
+		case '!':
+			return KEYBOARD_1;
+		case '2':
+		case '@':
+			return KEYBOARD_2;
+		case '3':
+		case '#':
+			return KEYBOARD_3;
+		case '4':
+		case '$':
+			return KEYBOARD_4;
+		case '5':
+		case '%':
+			return KEYBOARD_5;
+		case '6':
+		case '^':
+			return KEYBOARD_6;
+		case '7':
+		case '&':
+			return KEYBOARD_7;
+		case '8':
+		case '*':
+			return KEYBOARD_8;
+		case '9':
+		case '(':
+			return KEYBOARD_9;
+		case '0':
+		case ')':
+			return KEYBOARD_0;
+		case '-':
+		case '_':
+			return KEYBOARD_HYPHEN;
+		case '=':
+		case '+':
+			return KEYBOARD_EQUAL_SIGN;
+		case '\x7F':
+			return KEYBOARD_DELETE_OR_BACKSPACE;
+		case '\t':
+			return KEYBOARD_TAB;
+		case 'q':
+		case 'Q':
+			return KEYBOARD_Q;
+		case 'w':
+		case 'W':
+			return KEYBOARD_W;
+		case 'e':
+		case 'E':
+			return KEYBOARD_E;
+		case 'r':
+		case 'R':
+			return KEYBOARD_R;
+		case 't':
+		case 'T':
+			return KEYBOARD_T;
+		case 'y':
+		case 'Y':
+			return KEYBOARD_Y;
+		case 'u':
+		case 'U':
+			return KEYBOARD_U;
+		case 'i':
+		case 'I':
+			return KEYBOARD_I;
+		case 'o':
+		case 'O':
+			return KEYBOARD_O;
+		case 'p':
+		case 'P':
+			return KEYBOARD_P;
+		case '[':
+		case '{':
+			return KEYBOARD_OPEN_BRACKET;
+		case ']':
+		case '}':
+			return KEYBOARD_CLOSE_BRACKET;
+		case '\\':
+		case '|':
+			return KEYBOARD_BACKSLASH;
+		case 'a':
+		case 'A':
+			return KEYBOARD_A;
+		case 's':
+		case 'S':
+			return KEYBOARD_S;
+		case 'd':
+		case 'D':
+			return KEYBOARD_D;
+		case 'f':
+		case 'F':
+			return KEYBOARD_F;
+		case 'g':
+		case 'G':
+			return KEYBOARD_G;
+		case 'h':
+		case 'H':
+			return KEYBOARD_H;
+		case 'j':
+		case 'J':
+			return KEYBOARD_J;
+		case 'k':
+		case 'K':
+			return KEYBOARD_K;
+		case 'l':
+		case 'L':
+			return KEYBOARD_L;
+		case ';':
+		case ':':
+			return KEYBOARD_SEMICOLON;
+		case '\'':
+		case '"':
+			return KEYBOARD_QUOTE;
+		case '\r':
+		case '\n':
+			return KEYBOARD_RETURN_OR_ENTER;
+		case 'z':
+		case 'Z':
+			return KEYBOARD_Z;
+		case 'x':
+		case 'X':
+			return KEYBOARD_X;
+		case 'c':
+		case 'C':
+			return KEYBOARD_C;
+		case 'v':
+		case 'V':
+			return KEYBOARD_V;
+		case 'b':
+		case 'B':
+			return KEYBOARD_B;
+		case 'n':
+		case 'N':
+			return KEYBOARD_N;
+		case 'm':
+		case 'M':
+			return KEYBOARD_M;
+		case ',':
+		case '<':
+			return KEYBOARD_COMMA;
+		case '.':
+		case '>':
+			return KEYBOARD_PERIOD;
+		case '/':
+		case '?':
+			return KEYBOARD_SLASH;
+		case ' ':
+			return KEYBOARD_SPACEBAR;
+		default:
+			return 0;
+	}
+}
+
+- (BOOL) textField: (UITextField *) textField shouldChangeCharactersInRange: (NSRange) range replacementString: (NSString *) string {
+	unsigned int charIndex;
+	
+	if (range.length > 0 && [string length] == 0) {
+		Target_keyUp('\b', KEYBOARD_DELETE_OR_BACKSPACE);
+	} else {
+		for (charIndex = 0; charIndex < [string length]; charIndex++) {
+			Target_keyUp([string characterAtIndex: charIndex], unicharToShellKeyCode([string characterAtIndex: charIndex]));
+		}
+	}
+	
+	return NO;
+}
+
+- (BOOL) textFieldShouldReturn: (UITextField *) textField {
+	Target_keyUp('\n', KEYBOARD_RETURN_OR_ENTER);
+	return NO;
+}
+
+@end
