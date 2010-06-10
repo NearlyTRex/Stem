394036a8932aca133386ca281919c2ab2d36a6a4
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2010-06-10 20:23:46 +0000

    Tag stable template
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@538 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ template_stemlib/tags/Stable_2010-06-10/source/template_stemlib/HelloWorld.c
@@ -0,0 +1,9 @@
+#include "template_stemlib/HelloWorld.h"
+#include <stdlib.h>
+
+const char * helloWorld() {
+	void * foo;
+	
+	foo = malloc(1); // This is here just to generate an example analyzer warning
+	return "Hello, world!";
+}
