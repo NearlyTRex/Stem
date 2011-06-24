e1916c269e20a4cd793b8fb0df14b022406956b6
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2011-06-24 00:55:04 +0000

    Tag vorbisaudioio 1.0.0 source
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@849 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ vorbisaudioio/tags/1.0.0/source/vorbisaudioio/VorbisAudioIO.c
@@ -0,0 +1,110 @@
+/*
+  Copyright (c) 2011 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#include "vorbisaudioio/VorbisAudioIO.h"
+#include "vorbisaudioio/VorbisAudioIOPrivate.h"
+#include "vorbis/codec.h"
+#include "vorbis/vorbisfile.h"
+#include "utilities/IOUtilities.h"
+#include <string.h>
+
+PCMAudio * VorbisAudioIO_loadOggVorbisFile(const char * filePath) {
+	PCMAudio * audio;
+	void * fileContents;
+	size_t length = 0;
+	
+	fileContents = readFileSimple(filePath, &length);
+	audio = VorbisAudioIO_loadOggVorbisData(fileContents, length);
+	free(fileContents);
+	
+	return audio;
+}
+
+#define BUFFER_SIZE 16384
+
+PCMAudio * VorbisAudioIO_loadOggVorbisData(const void * data, size_t length) {
+	struct memreadContext readContext;
+	ov_callbacks callbacks;
+	OggVorbis_File file;
+	int status;
+	vorbis_info * info;
+	unsigned int channelCount;
+	unsigned int sampleRate;
+	unsigned int sampleCount;
+	long readResult;
+	struct memwriteContext writeContext;
+	char buffer[BUFFER_SIZE];
+	int currentSection;
+	PCMAudio * audio;
+	
+	readContext = memreadContextInit(data, length);
+	callbacks.read_func = VorbisAudioIO_memreadFunc;
+	callbacks.seek_func = VorbisAudioIO_memseekFunc;
+	callbacks.close_func = NULL;
+	callbacks.tell_func = VorbisAudioIO_memtellFunc;
+	status = ov_open_callbacks(&readContext, &file, NULL, 0, callbacks);
+	if (status != 0) {
+		fprintf(stderr, "Error: ov_open_callbacks returned %d\n", status);
+		return NULL;
+	}
+	
+	info = ov_info(&file, -1);
+	if (info == NULL) {
+		fprintf(stderr, "Error: ov_info returned NULL\n");
+		return NULL;
+	}
+	channelCount = info->channels;
+	sampleRate = info->rate;
+	
+	sampleCount = ov_pcm_total(&file, -1);
+	
+	writeContext = memwriteContextInit(malloc(1), 0, 1, true);
+	for (;;) {
+		readResult = ov_read(&file, buffer, BUFFER_SIZE, OV_READ_ENDIANNESS, 2, 1, &currentSection);
+		if (readResult == 0) {
+			break;
+		}
+		if (readResult < 0) {
+			fprintf(stderr, "Error: ov_read returned %ld\n", readResult);
+			break;
+		}
+		memwrite(&writeContext, readResult, buffer);
+	}
+	
+	ov_clear(&file);
+	if (readResult < 0) {
+		free(writeContext.data);
+		return NULL;
+	}
+	
+	audio = PCMAudio_create(2, channelCount, sampleRate, sampleCount, writeContext.data, true);
+	free(writeContext.data);
+	return audio;
+}
+
+bool VorbisAudioIO_writeOggVorbisFile(PCMAudio * audio, const char * filePath) {
+	return false;
+}
+
+void * VorbisAudioIO_writeOggVorbisData(PCMAudio * audio, size_t * outLength) {
+	return NULL;
+}
---
+++ vorbisaudioio/tags/1.0.0/test_source/testharness/TestHarness_main.c
@@ -0,0 +1,122 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "utilities/IOUtilities.h"
+#include "vorbisaudioio/VorbisAudioIO.h"
+#include "vorbisaudioio/VorbisAudioStream.h"
+
+static void printUsage() {
+	fprintf(stderr, "Usage: vorbisaudioio_testharness [-o outFile] [--stream] [--loop] inFile\n");
+}
+
+#define STREAM_READ_SIZE 16384
+
+int main(int argc, char ** argv) {
+	int argIndex;
+	char * inFileName = NULL;
+	char * outFileName = NULL;
+	bool stream = false;
+	bool loop = false;
+	bool data = false;
+	
+	for (argIndex = 1; argIndex < argc; argIndex++) {
+		if (!strcmp(argv[argIndex], "-o")) {
+			if (argIndex >= argc - 1) {
+				printUsage();
+				return EXIT_FAILURE;
+			}
+			outFileName = argv[++argIndex];
+			
+		} else if (!strcmp(argv[argIndex], "--stream")) {
+			stream = true;
+			
+		} else if (!strcmp(argv[argIndex], "--loop")) {
+			loop = true;
+			
+		} else if (!strcmp(argv[argIndex], "--data")) {
+			data = true;
+			
+		} else if (!strcmp(argv[argIndex], "--help")) {
+			printUsage();
+			return EXIT_SUCCESS;
+			
+		} else {
+			if (inFileName != NULL) {
+				printUsage();
+				return EXIT_FAILURE;
+			}
+			inFileName = argv[argIndex];
+		}
+	}
+	
+	if (inFileName == NULL) {
+		printUsage();
+		return EXIT_FAILURE;
+	}
+	
+	if (stream) {
+		VorbisAudioStream * stream;
+		
+		if (data) {
+			void * fileContents;
+			size_t fileLength;
+			
+			printf("Reading stream from memory\n");
+			fileContents = readFileSimple(inFileName, &fileLength);
+			stream = VorbisAudioStream_createWithData(fileContents, fileLength, true);
+		} else {
+			stream = VorbisAudioStream_createWithFile(inFileName);
+		}
+		printf("Stream has %zu samples\n%u bytes per sample\n%u channels\n%u samples per second\n", stream->sampleCount, stream->bytesPerSample, stream->channelCount, stream->sampleRate);
+		
+		if (outFileName != NULL) {
+			FILE * outFile;
+			size_t bytesToRead, bytesRead;
+			char buffer[STREAM_READ_SIZE];
+			
+			bytesToRead = stream->sampleCount * stream->channelCount * stream->bytesPerSample;
+			if (loop) {
+				bytesToRead *= 2;
+			}
+			
+			outFile = fopen(outFileName, "wb");
+			while (bytesToRead > 0) {
+				bytesRead = stream->read(stream, STREAM_READ_SIZE > bytesToRead ? bytesToRead : STREAM_READ_SIZE, buffer, loop);
+				if (bytesRead == 0) {
+					fprintf(stderr, "Unexpectedly couldn't read %zu more bytes\n", bytesToRead);
+					break;
+				}
+				printf("Read %zu bytes\n", bytesRead);
+				bytesToRead -= bytesRead;
+				fwrite(buffer, 1, bytesRead, outFile);
+			}
+			fclose(outFile);
+		}
+		
+		stream->dispose(stream);
+		
+	} else {
+		PCMAudio * audio;
+		
+		audio = VorbisAudioIO_loadOggVorbisFile(inFileName);
+		if (audio == NULL) {
+			return EXIT_FAILURE;
+		}
+		
+		printf("Read %zu samples\n%u bytes per sample\n%u channels\n%u samples per second\n", audio->sampleCount, audio->bytesPerSample, audio->channelCount, audio->sampleRate);
+		
+		if (outFileName != NULL) {
+			FILE * outFile;
+			
+			outFile = fopen(outFileName, "wb");
+			fwrite(audio->samples, 1, audio->sampleCount * audio->channelCount * audio->bytesPerSample, outFile);
+			fclose(outFile);
+		}
+		
+		audio->dispose(audio);
+	}
+	
+	return EXIT_SUCCESS;
+}
