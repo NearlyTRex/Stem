616cc7f7d4c3bf5c11edfb0a2c2a7c85980041b7
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2011-09-04 16:19:43 +0000

    Tag utilities 1.4.2 source
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@944 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ utilities/tags/1.4.2/source/utilities/EventDispatcher.c
@@ -0,0 +1,115 @@
+/*
+  Copyright (c) 2011 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#include "utilities/EventDispatcher.h"
+#include <string.h>
+
+struct EventTarget {
+	Atom eventID;
+	EventDispatcherCallback callback;
+	void * context;
+};
+
+EventDispatcher * EventDispatcher_create(void * owner) {
+	stemobject_create_implementation(EventDispatcher, init, owner)
+}
+
+void EventDispatcher_init(compat_type(EventDispatcher *) selfPtr, void * owner) {
+	EventDispatcher * self = selfPtr;
+	
+	StemObject_init(self);
+	
+	self->dispose = EventDispatcher_dispose;
+	self->registerForEvent = EventDispatcher_registerForEvent;
+	self->unregisterForEvent = EventDispatcher_unregisterForEvent;
+	self->dispatchEvent = EventDispatcher_dispatchEvent;
+	
+	self->owner = owner;
+	self->numberOfTargets = 0;
+	self->targetListSize = 1;
+	self->targets = (struct EventTarget *) malloc(sizeof(struct EventTarget) * self->targetListSize);
+}
+
+void EventDispatcher_dispose(compat_type(EventDispatcher *) selfPtr) {
+	EventDispatcher * self = selfPtr;
+	
+	free(self->targets);
+	StemObject_dispose(selfPtr);
+}
+
+void EventDispatcher_registerForEvent(compat_type(EventDispatcher *) selfPtr, Atom eventID, EventDispatcherCallback callback, void * context) {
+	EventDispatcher * self = selfPtr;
+	
+	if (self->numberOfTargets >= self->targetListSize) {
+		self->targetListSize *= 2;
+		self->targets = (struct EventTarget *) realloc(self->targets, sizeof(struct EventTarget) * self->targetListSize);
+	}
+	
+	self->targets[self->numberOfTargets].eventID = eventID;
+	self->targets[self->numberOfTargets].callback = callback;
+	self->targets[self->numberOfTargets].context = context;
+	self->numberOfTargets++;
+}
+
+void EventDispatcher_unregisterForEvent(compat_type(EventDispatcher *) selfPtr, Atom eventID, EventDispatcherCallback callback) {
+	EventDispatcher * self = selfPtr;
+	size_t targetIndex;
+	
+	for (targetIndex = 0; targetIndex < self->numberOfTargets; targetIndex++) {
+		// LEGACY: Should be able to pointer-compare Atoms; strcmp used for backward compatibility with utilities 1.4.1 and earlier
+		if (!strcmp(eventID, self->targets[targetIndex].eventID) && self->targets[targetIndex].callback == callback) {
+			self->numberOfTargets--;
+			for (; targetIndex < self->numberOfTargets; targetIndex++) {
+				self->targets[targetIndex] = self->targets[targetIndex + 1];
+			}
+			break;
+		}
+	}
+}
+
+bool EventDispatcher_dispatchEvent(compat_type(EventDispatcher *) selfPtr, Atom eventID, void * eventData) {
+	EventDispatcher * self = selfPtr;
+	size_t targetIndex;
+	size_t numberOfTargetsCopy;
+	struct EventTarget * targetsCopy;
+	bool eventHandled, anyEventsHandled;
+	
+	numberOfTargetsCopy = self->numberOfTargets;
+	targetsCopy = malloc(sizeof(struct EventTarget) * numberOfTargetsCopy);
+	memcpy(targetsCopy, self->targets, sizeof(struct EventTarget) * numberOfTargetsCopy);
+	
+	anyEventsHandled = false;
+	for (targetIndex = 0; targetIndex < numberOfTargetsCopy; targetIndex++) {
+		// LEGACY: Should be able to pointer-compare Atoms; strcmp used for backward compatibility with utilities 1.4.1 and earlier
+		if (!strcmp(eventID, self->targets[targetIndex].eventID)) {
+			eventHandled = targetsCopy[targetIndex].callback(self->owner, eventID, eventData, targetsCopy[targetIndex].context);
+			
+			if (eventHandled) {
+				anyEventsHandled = true;
+			}
+		}
+	}
+	
+	free(targetsCopy);
+	
+	return anyEventsHandled;
+}
---
+++ utilities/tags/1.4.2/test_source/unittest/suites/EventDispatcherTest.c
@@ -0,0 +1,70 @@
+#include "unittest/framework/TestSuite.h"
+#include "utilities/Atom.h"
+#include "utilities/EventDispatcher.h"
+
+static void * lastSender;
+
+static bool callback1(void * sender, Atom eventID, void * eventData, void * context) {
+	*(int *) context += *(int *) eventData;
+	TestCase_assert(eventID == Atom_fromString("event1"), "Expected %p \"event1\" but got %p \"%s\"", Atom_fromString("event1"), eventID, eventID);
+	lastSender = sender;
+	return true;
+}
+
+static bool callback2(void * sender, Atom eventID, void * eventData, void * context) {
+	*(int *) context += *(int *) eventData;
+	TestCase_assert(eventID == Atom_fromString("event2"), "Expected %p \"event2\" but got %p \"%s\"", Atom_fromString("event2"), eventID, eventID);
+	lastSender = sender;
+	return false;
+}
+
+static void testCallbacks() {
+	EventDispatcher * dispatcher;
+	char * sender1 = "1", * sender2 = "2";
+	int callback1Context = 0, callback2Context = 0;
+	int incrementation = 2;
+	bool handled;
+	Atom event1Atom, event2Atom;
+	
+	dispatcher = EventDispatcher_create(sender1);
+	
+	event1Atom = Atom_fromString("event1");
+	event2Atom = Atom_fromString("event2");
+	handled = dispatcher->dispatchEvent(dispatcher, event1Atom, &incrementation);
+	TestCase_assert(!handled, "Event handled when no callbacks are registered");
+	
+	dispatcher->registerForEvent(dispatcher, event1Atom, callback1, &callback1Context);
+	handled = dispatcher->dispatchEvent(dispatcher, event1Atom, &incrementation);
+	TestCase_assert(handled, "Event not handled when appropriate callback was registered");
+	TestCase_assert(callback1Context == 2, "Expected 2 but got %d", callback1Context);
+	TestCase_assert(lastSender == sender1, "Expected %p but got %p", sender1, lastSender);
+	
+	incrementation = 3;
+	dispatcher->dispatchEvent(dispatcher, event2Atom, &incrementation);
+	TestCase_assert(callback1Context == 2, "Callback called for wrong type of event");
+	
+	dispatcher->registerForEvent(dispatcher, event2Atom, callback2, &callback2Context);
+	handled = dispatcher->dispatchEvent(dispatcher, event2Atom, &incrementation);
+	TestCase_assert(callback2Context == 3, "Expected 3 but got %d", callback2Context);
+	TestCase_assert(!handled, "Expected false but got true");
+	
+	dispatcher->unregisterForEvent(dispatcher, event1Atom, callback1);
+	handled = dispatcher->dispatchEvent(dispatcher, event1Atom, &incrementation);
+	TestCase_assert(!handled, "Event still handled after unregistering callbacks");
+	
+	incrementation = 1;
+	handled = dispatcher->dispatchEvent(dispatcher, event2Atom, &incrementation);
+	TestCase_assert(callback2Context == 4, "Expected 4 but got %d", callback2Context);
+	TestCase_assert(!handled, "Expected false but got true");
+	
+	dispatcher->dispose(dispatcher);
+	
+	dispatcher = EventDispatcher_create(sender2);
+	dispatcher->registerForEvent(dispatcher, event1Atom, callback1, &callback1Context);
+	dispatcher->dispatchEvent(dispatcher, event1Atom, &incrementation);
+	TestCase_assert(lastSender == sender2, "Expected %p but got %p", sender2, lastSender);
+	
+	dispatcher->dispose(dispatcher);
+}
+
+TEST_SUITE(EventDispatcherTest, testCallbacks)
