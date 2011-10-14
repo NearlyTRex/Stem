531b1e2a8a36c58856e5d103b7648fffb7c9a9d6
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2011-10-14 04:10:31 +0000

    Tag jsonio 1.2.3 source
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@1027 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ jsonio/tags/1.2.3/source/jsonio/JSONIO.h
@@ -0,0 +1,97 @@
+/*
+  Copyright (c) 2011 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#ifndef __JSON_IO_H__
+#define __JSON_IO_H__
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+enum JSONType {
+	JSON_TYPE_OBJECT = 0,
+	JSON_TYPE_ARRAY = 1,
+	JSON_TYPE_STRING = 2,
+	JSON_TYPE_NUMBER = 3,
+	JSON_TYPE_BOOLEAN = 4,
+	JSON_TYPE_NULL = 5
+};
+
+#ifndef SIZE_T_MAX
+#define SIZE_T_MAX ((size_t) SIZE_MAX)
+#endif
+#define JSON_SUBITEM_NOT_FOUND SIZE_T_MAX
+
+struct JSONNode {
+	// Defined for all nodes
+	enum JSONType type;
+	
+	// Defined for all nodes that are subitems of an object node. NULL for all nodes that are not.
+	char * key;
+	
+	// Defined if key is defined. All strings are null-terminated, but can contain \u0000, so the actual length may be longer than the length measured by strlen.
+	size_t keyLength;
+	
+	// Defined for object and array nodes. The number of subitems is given by value.count. NULL for all other node types.
+	struct JSONNode * subitems;
+	
+	// Defined for string nodes. All strings are null-terminated, but can contain \u0000, so the actual length may be longer than the length measured by strlen.
+	size_t stringLength;
+	
+	union {
+		// Defined for object and array nodes
+		size_t count;
+		
+		// Defined for string nodes
+		char * string;
+		
+		// Defined for number nodes
+		double number;
+		
+		// Defined for boolean nodes
+		bool boolean;
+	} value;
+};
+
+/* Caller is responsible for freeing return value. */
+char * escapeJSONString(const char * string, size_t length, size_t * outLength);
+
+/* Returns NULL if string contains invalid escapes. Caller is responsible for freeing return value. */
+char * unescapeJSONString(const char * string, size_t length, size_t * outLength);
+
+// Returns JSON_SUBITEM_NOT_FOUND if no subitem with the specified key exists in objectNode, or if objectNode is not of JSON_TYPE_OBJECT
+size_t JSONNode_subitemIndexForKey(struct JSONNode * objectNode, const char * key, size_t keyLength);
+
+/* Frees node, its contents, and all subitems. */
+void JSONNode_dispose(struct JSONNode * node);
+
+/* Frees node's contents without freeing node itself */
+void JSONNode_disposeContents(struct JSONNode * node);
+
+/* Performs a deep copy of node. */
+struct JSONNode * JSONNode_copy(struct JSONNode * node);
+
+/* Performs a deep copy of node's contents, writing them to outNode. */
+void JSONNode_copyContents(struct JSONNode * node, struct JSONNode * outNode);
+
+#endif
---
+++ jsonio/tags/1.2.3/source/jsonio/JSONParser.c
@@ -0,0 +1,515 @@
+/*
+  Copyright (c) 2011 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#include "jsonio/JSONParser.h"
+#include "jsonio/JSONIO.h"
+#include "utilities/IOUtilities.h"
+#include "utilities/UTFUtilities.h"
+#include <stdio.h>
+#include <string.h>
+
+struct JSONNode * JSONParser_loadFile(const char * filePath, struct JSONParseError * outError) {
+	char * fileContents;
+	size_t fileLength = 0;
+	struct JSONNode * node;
+	
+	fileContents = readFileSimple(filePath, &fileLength);
+	if (fileContents == NULL) {
+		if (outError != NULL) {
+			outError->code = JSONParseError_fileNotFound;
+			outError->description = "File not found";
+		}
+		return NULL;
+	}
+	
+	node = JSONParser_loadString(fileContents, fileLength, outError);
+	
+	free(fileContents);
+	return node;
+}
+
+enum NumberToken {
+	NUMBER_TOKEN_INVALID,
+	NUMBER_TOKEN_PLUS,
+	NUMBER_TOKEN_MINUS,
+	NUMBER_TOKEN_DIGITS,
+	NUMBER_TOKEN_DECIMAL,
+	NUMBER_TOKEN_EXPONENT
+};
+
+static enum NumberToken nextNumberToken(const char * string, size_t length, size_t * ioCharIndex, size_t * outTokenLength) {
+	size_t charIndex;
+	
+	charIndex = *ioCharIndex;
+	
+	if (string[charIndex] == '-') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return NUMBER_TOKEN_MINUS;
+		
+	} else if (string[charIndex] == '+') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return NUMBER_TOKEN_PLUS;
+		
+	} else if (string[charIndex] == '.') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return NUMBER_TOKEN_DECIMAL;
+		
+	} else if (string[charIndex] == 'e' || string[charIndex] == 'E') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return NUMBER_TOKEN_EXPONENT;
+		
+	} else if (string[charIndex] >= '0' && string[charIndex] <= '9') {
+		size_t charIndex2;
+		
+		for (charIndex2 = charIndex + 1; charIndex2 < length; charIndex2++) {
+			if (string[charIndex2] < '0' || string[charIndex2] > '9') {
+				break;
+			}
+		}
+		*outTokenLength = charIndex2 - charIndex;
+		*ioCharIndex = charIndex2;
+		return NUMBER_TOKEN_DIGITS;
+	}
+	
+	*outTokenLength = 0;
+	return NUMBER_TOKEN_INVALID;
+}
+
+enum JSONToken {
+	JSON_TOKEN_INVALID,
+	JSON_TOKEN_BEGIN_ARRAY,
+	JSON_TOKEN_BEGIN_OBJECT,
+	JSON_TOKEN_END_ARRAY,
+	JSON_TOKEN_END_OBJECT,
+	JSON_TOKEN_COLON,
+	JSON_TOKEN_COMMA,
+	JSON_TOKEN_NULL,
+	JSON_TOKEN_TRUE,
+	JSON_TOKEN_FALSE,
+	JSON_TOKEN_NUMBER,
+	JSON_TOKEN_STRING,
+	JSON_TOKEN_EOF
+};
+
+static enum JSONToken nextJSONToken(const char * string, size_t length, size_t * ioCharIndex, size_t * outTokenLength) {
+	size_t charIndex;
+	
+	charIndex = *ioCharIndex;
+	while (charIndex < length && (string[charIndex] == ' ' || string[charIndex] == '\t' || string[charIndex] == '\r' || string[charIndex] == '\n')) {
+		charIndex++;
+	}
+	*ioCharIndex = charIndex;
+	
+	if (charIndex >= length) {
+		*outTokenLength = 0;
+		return JSON_TOKEN_EOF;
+	}
+	
+	if (string[charIndex] == '[') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return JSON_TOKEN_BEGIN_ARRAY;
+		
+	} else if (string[charIndex] == '{') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return JSON_TOKEN_BEGIN_OBJECT;
+		
+	} else if (string[charIndex] == ']') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return JSON_TOKEN_END_ARRAY;
+		
+	} else if (string[charIndex] == '}') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return JSON_TOKEN_END_OBJECT;
+		
+	} else if (string[charIndex] == ':') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return JSON_TOKEN_COLON;
+		
+	} else if (string[charIndex] == ',') {
+		*outTokenLength = 1;
+		*ioCharIndex = charIndex + 1;
+		return JSON_TOKEN_COMMA;
+		
+	} else if (string[charIndex] == 'n') {
+		if (charIndex + 4 <= length && !strncmp(string + charIndex, "null", 4)) {
+			*outTokenLength = 4;
+			*ioCharIndex = charIndex + 4;
+			return JSON_TOKEN_NULL;
+			
+		} else {
+			*outTokenLength = 0;
+			return JSON_TOKEN_INVALID;
+		}
+		
+	} else if (string[charIndex] == 't') {
+		if (charIndex + 4 <= length && !strncmp(string + charIndex, "true", 4)) {
+			*outTokenLength = 4;
+			*ioCharIndex = charIndex + 4;
+			return JSON_TOKEN_TRUE;
+			
+		} else {
+			*outTokenLength = 0;
+			return JSON_TOKEN_INVALID;
+		}
+		
+	} else if (string[charIndex] == 'f') {
+		if (charIndex + 5 <= length && !strncmp(string + charIndex, "false", 5)) {
+			*outTokenLength = 5;
+			*ioCharIndex = charIndex + 5;
+			return JSON_TOKEN_FALSE;
+			
+		} else {
+			*outTokenLength = 0;
+			return JSON_TOKEN_INVALID;
+		}
+		
+	} else if (string[charIndex] == '"') {
+		bool escaped = false;
+		size_t charIndex2;
+		
+		for (charIndex2 = charIndex + 1; charIndex2 < length; charIndex2++) {
+			if (string[charIndex2] >= '\0' && string[charIndex2] < ' ' && string[charIndex2] != '\t') {
+				break;
+			}
+			
+			if (escaped) {
+				escaped = false;
+				
+			} else {
+				if (string[charIndex2] == '\\') {
+					escaped = true;
+					
+				} else if (string[charIndex2] == '"') {
+					*outTokenLength = charIndex2 - charIndex + 1;
+					*ioCharIndex = charIndex2 + 1;
+					return JSON_TOKEN_STRING;
+				}
+			}
+		}
+		
+	} else if (string[charIndex] == '-' || (string[charIndex] >= '0' && string[charIndex] <= '9')) {
+		enum NumberToken token;
+		size_t charIndex2, tokenLength;
+		
+		charIndex2 = charIndex;
+		
+		token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+		if (token == NUMBER_TOKEN_MINUS) {
+			token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+		}
+		
+		if (token == NUMBER_TOKEN_DIGITS && (tokenLength == 1 || string[charIndex2 - tokenLength] != '0')) {
+			token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+			if (token == NUMBER_TOKEN_INVALID) {
+				*outTokenLength = charIndex2 - charIndex;
+				*ioCharIndex = charIndex2;
+				return JSON_TOKEN_NUMBER;
+			}
+			
+			if (token == NUMBER_TOKEN_DECIMAL) {
+				token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+				if (token == NUMBER_TOKEN_DIGITS) {
+					token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+					if (token == NUMBER_TOKEN_INVALID) {
+						*outTokenLength = charIndex2 - charIndex;
+						*ioCharIndex = charIndex2;
+						return JSON_TOKEN_NUMBER;
+					}
+					
+					if (token == NUMBER_TOKEN_EXPONENT) {
+						token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+						if (token == NUMBER_TOKEN_PLUS || token == NUMBER_TOKEN_MINUS) {
+							token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+						}
+						
+						if (token == NUMBER_TOKEN_DIGITS) {
+							token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+							if (token == NUMBER_TOKEN_INVALID) {
+								*outTokenLength = charIndex2 - charIndex;
+								*ioCharIndex = charIndex2;
+								return JSON_TOKEN_NUMBER;
+							}
+						}
+					}
+				}
+				
+			} else if (token == NUMBER_TOKEN_EXPONENT) {
+				token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+				if (token == NUMBER_TOKEN_PLUS || token == NUMBER_TOKEN_MINUS) {
+					token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+				}
+				
+				if (token == NUMBER_TOKEN_DIGITS) {
+					token = nextNumberToken(string, length, &charIndex2, &tokenLength);
+					if (token == NUMBER_TOKEN_INVALID) {
+						*outTokenLength = charIndex2 - charIndex;
+						*ioCharIndex = charIndex2;
+						return JSON_TOKEN_NUMBER;
+					}
+				}
+			}
+		}
+	}
+	
+	*outTokenLength = 0;
+	return JSON_TOKEN_INVALID;
+}
+
+struct JSONNode * JSONParser_loadString(const char * string, size_t length, struct JSONParseError * outError) {
+	struct JSONNode * rootNode, * containerNode, node;
+	struct JSONNode nodePrototype;
+	size_t charIndex = 0, tokenLength;
+	enum JSONToken token;
+	struct JSONNode ** nodeStack;
+	size_t nodeStackAllocatedSize, nodeStackCurrentDepth;
+	
+	memset(&nodePrototype, 0, sizeof(nodePrototype));
+	nodePrototype.type = JSON_TYPE_NULL;
+	rootNode = malloc(sizeof(struct JSONNode));
+	*rootNode = nodePrototype;
+	
+	token = nextJSONToken(string, length, &charIndex, &tokenLength);
+	if (token == JSON_TOKEN_BEGIN_ARRAY) {
+		rootNode->type = JSON_TYPE_ARRAY;
+		
+	} else if (token == JSON_TOKEN_BEGIN_OBJECT) {
+		rootNode->type = JSON_TYPE_OBJECT;
+		
+	} else {
+		JSONNode_dispose(rootNode);
+		if (outError != NULL) {
+			outError->charIndex = charIndex - tokenLength;
+			outError->code = JSONParseError_rootNodeNotFound;
+			outError->description = "Didn't find \"[\" or \"{\" at beginning of document";
+		}
+		return NULL;
+	}
+	
+	nodeStackAllocatedSize = 1;
+	nodeStack = malloc(sizeof(struct JSONNode *) * nodeStackAllocatedSize);
+	nodeStackCurrentDepth = 0;
+	containerNode = rootNode;
+	
+	for (;;) {
+		token = nextJSONToken(string, length, &charIndex, &tokenLength);
+		if ((token == JSON_TOKEN_END_ARRAY && containerNode->type == JSON_TYPE_ARRAY) ||
+		    (token == JSON_TOKEN_END_OBJECT && containerNode->type == JSON_TYPE_OBJECT)) {
+			if (nodeStackCurrentDepth == 0) {
+				break;
+			}
+			
+			nodeStackCurrentDepth--;
+			containerNode = nodeStack[nodeStackCurrentDepth];
+			
+			continue;
+		}
+		
+		if (containerNode->value.count > 0) {
+			if (token != JSON_TOKEN_COMMA) {
+				JSONNode_dispose(rootNode);
+				free(nodeStack);
+				if (outError != NULL) {
+					outError->charIndex = charIndex - tokenLength;
+					if (token == JSON_TOKEN_EOF) {
+						outError->code = JSONParseError_unexpectedEOF;
+						outError->description = "Unexpected end of file when seeking comma";
+					} else {
+						outError->code = JSONParseError_unexpectedToken;
+						outError->description = "Unexpected token when seeking comma";
+					}
+				}
+				return NULL;
+			}
+			token = nextJSONToken(string, length, &charIndex, &tokenLength);
+			
+		} else if (token == JSON_TOKEN_COMMA) {
+			JSONNode_dispose(rootNode);
+			free(nodeStack);
+			if (outError != NULL) {
+				outError->charIndex = charIndex - tokenLength;
+				outError->code = JSONParseError_unexpectedToken;
+				if (containerNode->type == JSON_TYPE_OBJECT) {
+					outError->description = "Unexpected comma when seeking key";
+				} else {
+					outError->description = "Unexpected comma when seeking value";
+				}
+			}
+			return NULL;
+		}
+		
+		node = nodePrototype;
+		
+		if (containerNode->type == JSON_TYPE_OBJECT) {
+			if (token != JSON_TOKEN_STRING) {
+				JSONNode_dispose(rootNode);
+				free(nodeStack);
+				if (outError != NULL) {
+					outError->charIndex = charIndex - tokenLength;
+					if (charIndex >= length) {
+						outError->code = JSONParseError_unexpectedEOF;
+						outError->description = "Unexpected end of file when seeking object key";
+					} else {
+						outError->code = JSONParseError_objectKeyNotFound;
+						outError->description = "Didn't get object key string when expected";
+					}
+				}
+				return NULL;
+			}
+			
+			node.key = unescapeJSONString(string + charIndex - tokenLength + 1, tokenLength - 2, &node.keyLength);
+			if (node.key == NULL) {
+				JSONNode_dispose(rootNode);
+				free(nodeStack);
+				if (outError != NULL) {
+					outError->charIndex = charIndex - tokenLength;
+					outError->code = JSONParseError_malformedString;
+					outError->description = "Couldn't unescape key string";
+				}
+				return NULL;
+			}
+			
+			token = nextJSONToken(string, length, &charIndex, &tokenLength);
+			if (token != JSON_TOKEN_COLON) {
+				JSONNode_disposeContents(&node);
+				JSONNode_dispose(rootNode);
+				free(nodeStack);
+				if (outError != NULL) {
+					outError->charIndex = charIndex - tokenLength;
+					outError->code = JSONParseError_keyNotFollowedByColon;
+					outError->description = "Object key not followed by colon";
+				}
+				return NULL;
+			}
+			token = nextJSONToken(string, length, &charIndex, &tokenLength);
+		}
+		
+		if (token == JSON_TOKEN_NULL) {
+			node.type = JSON_TYPE_NULL;
+			containerNode->value.count++;
+			containerNode->subitems = realloc(containerNode->subitems, sizeof(struct JSONNode) * containerNode->value.count);
+			containerNode->subitems[containerNode->value.count - 1] = node;
+			
+		} else if (token == JSON_TOKEN_FALSE) {
+			node.type = JSON_TYPE_BOOLEAN;
+			node.value.boolean = false;
+			containerNode->value.count++;
+			containerNode->subitems = realloc(containerNode->subitems, sizeof(struct JSONNode) * containerNode->value.count);
+			containerNode->subitems[containerNode->value.count - 1] = node;
+			
+		} else if (token == JSON_TOKEN_TRUE) {
+			node.type = JSON_TYPE_BOOLEAN;
+			node.value.boolean = true;
+			containerNode->value.count++;
+			containerNode->subitems = realloc(containerNode->subitems, sizeof(struct JSONNode) * containerNode->value.count);
+			containerNode->subitems[containerNode->value.count - 1] = node;
+			
+		} else if (token == JSON_TOKEN_NUMBER) {
+			static char * strtodBuffer;
+			static size_t strtodBufferSize;
+			
+			// Windows XP's implementation of strtod can crash on non-null-terminated strings, even ones that contain non-numeric characters after the number
+			if (tokenLength >= strtodBufferSize) {
+				strtodBufferSize = tokenLength + 1;
+				strtodBuffer = realloc(strtodBuffer, strtodBufferSize);
+			}
+			memcpy(strtodBuffer, string + charIndex - tokenLength, tokenLength);
+			strtodBuffer[tokenLength] = 0;
+			node.type = JSON_TYPE_NUMBER;
+			node.value.number = strtod(strtodBuffer, NULL);
+			containerNode->value.count++;
+			containerNode->subitems = realloc(containerNode->subitems, sizeof(struct JSONNode) * containerNode->value.count);
+			containerNode->subitems[containerNode->value.count - 1] = node;
+			
+		} else if (token == JSON_TOKEN_STRING) {
+			node.type = JSON_TYPE_STRING;
+			node.value.string = unescapeJSONString(string + charIndex - tokenLength + 1, tokenLength - 2, &node.stringLength);
+			if (node.value.string == NULL) {
+				JSONNode_disposeContents(&node);
+				JSONNode_dispose(rootNode);
+				free(nodeStack);
+				if (outError != NULL) {
+					outError->charIndex = charIndex - tokenLength;
+					outError->code = JSONParseError_malformedString;
+					outError->description = "Couldn't unescape value string";
+				}
+				return NULL;
+			}
+			containerNode->value.count++;
+			containerNode->subitems = realloc(containerNode->subitems, sizeof(struct JSONNode) * containerNode->value.count);
+			containerNode->subitems[containerNode->value.count - 1] = node;
+			
+		} else if (token == JSON_TOKEN_BEGIN_ARRAY || token == JSON_TOKEN_BEGIN_OBJECT) {
+			node.type = token == JSON_TOKEN_BEGIN_ARRAY ? JSON_TYPE_ARRAY : JSON_TYPE_OBJECT;
+			if (nodeStackAllocatedSize <= nodeStackCurrentDepth) {
+				nodeStackAllocatedSize *= 2;
+				nodeStack = realloc(nodeStack, sizeof(struct JSONNode *) * nodeStackAllocatedSize);
+			}
+			nodeStack[nodeStackCurrentDepth++] = containerNode;
+			containerNode->value.count++;
+			containerNode->subitems = realloc(containerNode->subitems, sizeof(struct JSONNode) * containerNode->value.count);
+			containerNode->subitems[containerNode->value.count - 1] = node;
+			
+			containerNode = &containerNode->subitems[containerNode->value.count - 1];
+			
+		} else {
+			JSONNode_disposeContents(&node);
+			JSONNode_dispose(rootNode);
+			free(nodeStack);
+			if (outError != NULL) {
+				outError->charIndex = charIndex - tokenLength;
+				if (charIndex >= length) {
+					outError->code = JSONParseError_unexpectedEOF;
+					outError->description = "Unexpected end of file when seeking value";
+				} else {
+					outError->code = JSONParseError_unexpectedToken;
+					outError->description = "Unexpected token when seeking value";
+				}
+			}
+			return NULL;
+		}
+	}
+	free(nodeStack);
+	
+	while (charIndex < length && (string[charIndex] == ' ' || string[charIndex] == '\t' || string[charIndex] == '\r' || string[charIndex] == '\n')) {
+		charIndex++;
+	}
+	if (charIndex != length) {
+		JSONNode_dispose(rootNode);
+		if (outError != NULL) {
+			outError->charIndex = charIndex;
+			outError->code = JSONParseError_unexpectedToken;
+			outError->description = "Extra data after expected end of file";
+		}
+		return NULL;
+	}
+	
+	return rootNode;
+}
