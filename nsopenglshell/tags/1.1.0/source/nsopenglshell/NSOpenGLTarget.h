48fae9c63f7b3b2f484b86575e4bced6ceeaccfb
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2012-02-11 20:49:17 +0000

    Tag nsopenglshell 1.1.0 source
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@1161 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ nsopenglshell/tags/1.1.0/source/nsopenglshell/NSOpenGLTarget.h
@@ -0,0 +1,33 @@
+/*
+  Copyright (c) 2012 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#ifndef __NSOPENGL_TARGET_H__
+#define __NSOPENGL_TARGET_H__
+
+#include "nsopenglshell/NSOpenGLShell.h"
+
+/** Called prior to Target_init() to allow the target to express preferences for various settings related
+    to OpenGL context setup and other things. See the NSOpenGLShellConfiguration struct in NSOpenGLShell.h
+    for details. */
+void NSOpenGLTarget_configure(int argc, const char ** argv, struct NSOpenGLShellConfiguration * configuration);
+
+#endif
