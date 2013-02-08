0a8173b347300a8a8152e2f55838191e7909f45c
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2013-02-08 09:39:21 +0000

    Suppress gcc warning
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@1379 d291a34c-4c4d-0410-8556-a73de5c6a500

--- template_stemlib/trunk/source/template_stemlib/HelloWorld.c
+++ template_stemlib/trunk/source/template_stemlib/HelloWorld.c
@@ -1,9 +1,13 @@
 #include "template_stemlib/HelloWorld.h"
 #include <stdlib.h>
 
+void bar(void * ptr) {
+}
+
 const char * helloWorld() {
 	void * foo;
 	
 	foo = malloc(1); // This is here just to generate an example analyzer warning
+	bar(foo); // Suppress gcc warning for unused foo
 	return "Hello, world!";
 }
