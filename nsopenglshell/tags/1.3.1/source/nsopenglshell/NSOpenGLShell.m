0df5951e0889f3c14b0946c091804e2084c3cd34
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2013-12-26 03:29:14 +0000

    Tag nsopenglshell 1.3.1 source
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@1648 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ nsopenglshell/tags/1.3.1/source/nsopenglshell/NSOpenGLShell.m
@@ -0,0 +1,438 @@
+/*
+  Copyright (c) 2013 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#include "nsopenglshell/NSOpenGLShell.h"
+
+#import <Cocoa/Cocoa.h>
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#import <IOKit/ps/IOPowerSources.h>
+#import <IOKit/ps/IOPSKeys.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+
+#import "nsopenglshell/NSOpenGLShellApplication.h"
+#include "shell/Shell.h"
+#include "shell/ShellBatteryInfo.h"
+#include "shell/ShellThreads.h"
+
+@interface NSOpenGLShellTimerTarget : NSObject {
+	unsigned int timerID;
+	bool repeat;
+	void (* callback)(unsigned int timerID, void * context);
+	void * context;
+}
+@end
+@implementation NSOpenGLShellTimerTarget
+- (id) initWithID: (unsigned int) inTimerID repeat: (bool) inRepeat callback: (void (*)(unsigned int timerID, void * context)) inCallback context: (void *) inContext {
+	if ((self = [super init]) != nil) {
+		timerID = inTimerID;
+		repeat = inRepeat;
+		callback = inCallback;
+		context = inContext;
+	}
+	return self;
+}
+
+- (void) timer: (NSTimer *) timer {
+	callback(timerID, context);
+	if (!repeat) {
+		Shell_cancelTimer(timerID);
+	}
+}
+@end
+
+struct NSOpenGLShellTimer {
+	unsigned int id;
+	NSTimer * timer;
+	NSOpenGLShellTimerTarget * timerTarget;
+};
+
+bool mainLoopCalled = false;
+static bool cursorHiddenByHide = false;
+static unsigned int nextTimerID;
+static size_t timerCount;
+static struct NSOpenGLShellTimer * timers;
+
+void Shell_mainLoop() {
+	mainLoopCalled = true;
+}
+
+void Shell_redisplay() {
+	[(NSOpenGLShellApplication *) [NSApplication sharedApplication] redisplayPosted];
+}
+
+bool Shell_isFullScreen() {
+	return [(NSOpenGLShellApplication *) [NSApplication sharedApplication] isFullScreen];
+}
+
+bool Shell_setFullScreen(bool fullScreen) {
+	if (fullScreen != Shell_isFullScreen()) {
+		[(NSOpenGLShellApplication *) [NSApplication sharedApplication] toggleFullScreen];
+	}
+	return true;
+}
+
+double Shell_getCurrentTime() {
+	static mach_timebase_info_data_t timebaseInfo;
+	
+	if (timebaseInfo.denom == 0) {
+		mach_timebase_info(&timebaseInfo);
+	}
+	return mach_absolute_time() * (double) timebaseInfo.numer / timebaseInfo.denom * 0.000000001;
+}
+
+const char * Shell_getResourcePath() {
+	static char resourcePath[PATH_MAX];
+	CFBundleRef mainBundle;
+	CFURLRef resourcesDirectoryURL;
+	
+	mainBundle = CFBundleGetMainBundle();
+	resourcesDirectoryURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+	CFURLGetFileSystemRepresentation(resourcesDirectoryURL, true, (UInt8 *) resourcePath, PATH_MAX);
+	CFRelease(resourcesDirectoryURL);
+	
+	return resourcePath;
+}
+
+const char * Shell_getSupportPath(const char * subdirectory) {
+	static char supportPath[PATH_MAX];
+	NSArray * paths;
+	
+	paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+	if (subdirectory == NULL) {
+		strncpy(supportPath, [[paths objectAtIndex: 0] UTF8String], PATH_MAX);
+	} else {
+		snprintf(supportPath, PATH_MAX, "%s/%s", [[paths objectAtIndex: 0] UTF8String], subdirectory);
+	}
+	mkdir(supportPath, 0777);
+	
+	return supportPath;
+}
+
+enum ShellBatteryState Shell_getBatteryState() {
+	CFTypeRef info;
+	CFArrayRef list;
+	CFDictionaryRef battery;
+	enum ShellBatteryState batteryState = ShellBatteryState_notBatteryPowered;
+	
+	info = IOPSCopyPowerSourcesInfo();
+	if (info == NULL) {
+		return batteryState;
+	}
+	list = IOPSCopyPowerSourcesList(info);
+	if (list == NULL) {
+		CFRelease(info);
+		return batteryState;
+	}
+	
+	if (CFArrayGetCount(list) > 0 && (battery = IOPSGetPowerSourceDescription(info, CFArrayGetValueAtIndex(list, 0))) != NULL) {
+		if (![[(NSDictionary *) battery objectForKey: @kIOPSIsPresentKey] boolValue]) {
+			batteryState = ShellBatteryState_batteryMissing; // How to tell it shouldn't be ShellBatteryState_notBatteryPowered?
+		} else if ([[(NSDictionary *) battery objectForKey: @kIOPSIsChargingKey] boolValue]) {
+			batteryState = ShellBatteryState_charging;
+		} else if ([(NSString *) [(NSDictionary *) battery objectForKey: @kIOPSPowerSourceStateKey] isEqualToString: @kIOPSACPowerValue]) {
+			batteryState = ShellBatteryState_full;
+		} else {
+			batteryState = ShellBatteryState_unplugged;
+		}
+	}
+	
+	CFRelease(list);
+	CFRelease(info);
+	
+	return batteryState;
+}
+
+float Shell_getBatteryLevel() {
+	CFTypeRef info;
+	CFArrayRef list;
+	CFDictionaryRef battery;
+	float batteryLevel = -1.0f;
+	
+	info = IOPSCopyPowerSourcesInfo();
+	if (info == NULL) {
+		return -1.0f;
+	}
+	list = IOPSCopyPowerSourcesList(info);
+	if (list == NULL) {
+		CFRelease(info);
+		return -1.0f;
+	}
+	
+	if (CFArrayGetCount(list) > 0 && (battery = IOPSGetPowerSourceDescription(info, CFArrayGetValueAtIndex(list, 0))) != NULL) {
+		if ([[(NSDictionary *) battery objectForKey: @kIOPSIsPresentKey] boolValue]) {
+			batteryLevel = [[(NSDictionary *) battery objectForKey: @kIOPSCurrentCapacityKey] floatValue] / [[(NSDictionary *) battery objectForKey: @kIOPSMaxCapacityKey] floatValue];
+		}
+	}
+	
+	CFRelease(list);
+	CFRelease(info);
+	
+	return batteryLevel;
+}
+
+void Shell_getMainScreenSize(unsigned int * outWidth, unsigned int * outHeight) {
+	NSRect bounds;
+	
+	bounds = [[NSScreen mainScreen] frame];
+	if (outWidth != NULL) {
+		*outWidth = bounds.size.width;
+	}
+	if (outHeight != NULL) {
+		*outHeight = bounds.size.height;
+	}
+}
+
+unsigned int Shell_setTimer(double interval, bool repeat, void (* callback)(unsigned int timerID, void * context), void * context) {
+	timers = realloc(timers, sizeof(struct NSOpenGLShellTimer) * (timerCount + 1));
+	timers[timerCount].id = nextTimerID++;
+	timers[timerCount].timerTarget = [[NSOpenGLShellTimerTarget alloc] initWithID: timers[timerCount].id repeat: repeat callback: callback context: context];
+	timers[timerCount].timer = [NSTimer scheduledTimerWithTimeInterval: interval target: timers[timerCount].timerTarget selector: @selector(timer:) userInfo: nil repeats: repeat];
+	return timers[timerCount++].id;
+}
+
+void Shell_cancelTimer(unsigned int timerID) {
+	unsigned int timerIndex;
+	
+	for (timerIndex = 0; timerIndex < timerCount; timerIndex++) {
+		if (timers[timerIndex].id == timerID) {
+			[timers[timerIndex].timer invalidate];
+			[timers[timerIndex].timerTarget release];
+			timerCount--;
+			for (; timerIndex < timerCount; timerIndex++) {
+				timers[timerIndex] = timers[timerIndex + 1];
+			}
+			break;
+		}
+	}
+}
+
+void Shell_setCursorVisible(bool visible) {
+	if (visible) {
+		if (cursorHiddenByHide) {
+			cursorHiddenByHide = false;
+			if (!g_mouseDeltaMode) {
+				[NSCursor unhide];
+			}
+		}
+	} else {
+		if (!cursorHiddenByHide) {
+			cursorHiddenByHide = true;
+			[NSCursor hide];
+		}
+	}
+}
+
+void Shell_hideCursorUntilMouseMoves() {
+	[NSCursor setHiddenUntilMouseMoves: YES];
+}
+
+@interface NSCursor (MacOSX10_6Cursors)
++ (NSCursor *)contextualMenuCursor;
++ (NSCursor *)dragCopyCursor;
++ (NSCursor *)dragLinkCursor;
++ (NSCursor *)operationNotAllowedCursor;
+@end
+
+void Shell_setCursor(int cursor) {
+	switch (cursor) {
+		case ShellCursor_arrow:
+			[[NSCursor arrowCursor] set];
+			break;
+		case ShellCursor_iBeam:
+			[[NSCursor IBeamCursor] set];
+			break;
+		case ShellCursor_crosshair:
+			[[NSCursor crosshairCursor] set];
+			break;
+		case ShellCursor_hand:
+			[[NSCursor pointingHandCursor] set];
+			break;
+		case NSOpenGLShellCursor_closedHand:
+			[[NSCursor closedHandCursor] set];
+			break;
+		case NSOpenGLShellCursor_openHand:
+			[[NSCursor openHandCursor] set];
+			break;
+		case NSOpenGLShellCursor_resizeLeft:
+			[[NSCursor resizeLeftCursor] set];
+			break;
+		case NSOpenGLShellCursor_resizeRight:
+			[[NSCursor resizeRightCursor] set];
+			break;
+		case NSOpenGLShellCursor_resizeLeftRight:
+			[[NSCursor resizeLeftRightCursor] set];
+			break;
+		case NSOpenGLShellCursor_resizeUp:
+			[[NSCursor resizeUpCursor] set];
+			break;
+		case NSOpenGLShellCursor_resizeDown:
+			[[NSCursor resizeDownCursor] set];
+			break;
+		case NSOpenGLShellCursor_resizeUpDown:
+			[[NSCursor resizeUpDownCursor] set];
+			break;
+		case NSOpenGLShellCursor_disappearingItem:
+			[[NSCursor disappearingItemCursor] set];
+			break;
+		case NSOpenGLShellCursor_contextualMenu:
+			if ([NSCursor respondsToSelector: @selector(contextualMenuCursor)]) {
+				[[NSCursor contextualMenuCursor] set];
+			}
+			break;
+		case NSOpenGLShellCursor_dragCopy:
+			if ([NSCursor respondsToSelector: @selector(dragCopyCursor)]) {
+				[[NSCursor dragCopyCursor] set];
+			}
+			break;
+		case NSOpenGLShellCursor_dragLink:
+			if ([NSCursor respondsToSelector: @selector(dragLinkCursor)]) {
+				[[NSCursor dragLinkCursor] set];
+			}
+			break;
+		case NSOpenGLShellCursor_operationNotAllowed:
+			if ([NSCursor respondsToSelector: @selector(operationNotAllowedCursor)]) {
+				[[NSCursor operationNotAllowedCursor] set];
+			}
+			break;
+	}
+}
+
+void Shell_setMouseDeltaMode(bool deltaMode) {
+	if (g_mouseDeltaMode != deltaMode) {
+		g_mouseDeltaMode = deltaMode;
+		if (!cursorHiddenByHide) {
+			if (deltaMode) {
+				[NSCursor hide];
+			} else {
+				[NSCursor unhide];
+			}
+		}
+		CGAssociateMouseAndMouseCursorPosition(!deltaMode);
+	}
+}
+
+struct threadFuncInvocation {
+	int (* threadFunction)(void * context);
+	void * context;
+};
+
+static void * callThreadFunc(void * context) {
+	struct threadFuncInvocation * invocation = context;
+	int (* threadFunction)(void * context);
+	void * threadContext;
+	
+	threadFunction  = invocation->threadFunction;
+	threadContext = invocation->context;
+	free(invocation);
+	return (void *) threadFunction(threadContext);
+}
+
+ShellThread Shell_createThread(int (* threadFunction)(void * context), void * context) {
+	pthread_t thread;
+	struct threadFuncInvocation * invocation;
+	
+	invocation = malloc(sizeof(struct threadFuncInvocation));
+	invocation->threadFunction = threadFunction;
+	invocation->context = context;
+	pthread_create(&thread, NULL, callThreadFunc, invocation);
+	return (ShellThread) thread;
+}
+
+void Shell_detachThread(ShellThread thread) {
+	pthread_detach((pthread_t) thread);
+}
+
+int Shell_joinThread(ShellThread thread) {
+	int status;
+	void * returnValue;
+	
+	status = pthread_join((pthread_t) thread, &returnValue);
+	if (status == 0) {
+		return (int) returnValue;
+	}
+	return status;
+}
+
+void Shell_exitThread(int statusCode) {
+	pthread_exit((void *) statusCode);
+}
+
+ShellThread Shell_getCurrentThread() {
+	return (ShellThread) pthread_self();
+}
+
+ShellMutex Shell_createMutex() {
+	pthread_mutex_t * mutex;
+	pthread_mutexattr_t recursiveLock;
+
+	pthread_mutexattr_init(&recursiveLock);
+	pthread_mutexattr_settype(&recursiveLock, PTHREAD_MUTEX_RECURSIVE);
+	mutex = malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(mutex, &recursiveLock);
+	pthread_mutexattr_destroy(&recursiveLock);
+	return (ShellMutex) mutex;
+}
+
+void Shell_disposeMutex(ShellMutex mutex) {
+	pthread_mutex_destroy((pthread_mutex_t *) mutex);
+	free(mutex);
+}
+
+void Shell_lockMutex(ShellMutex mutex) {
+	pthread_mutex_lock((pthread_mutex_t *) mutex);
+}
+
+bool Shell_tryLockMutex(ShellMutex mutex) {
+	return !pthread_mutex_trylock((pthread_mutex_t *) mutex);
+}
+
+void Shell_unlockMutex(ShellMutex mutex) {
+	pthread_mutex_unlock((pthread_mutex_t *) mutex);
+}
+
+ShellSemaphore Shell_createSemaphore(unsigned int value) {
+	sem_t * semaphore;
+	
+	semaphore = malloc(sizeof(sem_t));
+	sem_init(semaphore, 0, value > SEM_VALUE_MAX ? SEM_VALUE_MAX : value);
+	return (ShellSemaphore) semaphore;
+}
+
+void Shell_disposeSemaphore(ShellSemaphore semaphore) {
+	sem_destroy((sem_t *) semaphore);
+	free(semaphore);
+}
+
+void Shell_postSemaphore(ShellSemaphore semaphore) {
+	sem_post((sem_t *) semaphore);
+}
+
+void Shell_waitSemaphore(ShellSemaphore semaphore) {
+	sem_wait((sem_t *) semaphore);
+}
+
+bool Shell_tryWaitSemaphore(ShellSemaphore semaphore) {
+	return !sem_trywait((sem_t *) semaphore);
+}
---
+++ nsopenglshell/tags/1.3.1/source/testharness/TestHarness_main.c
@@ -0,0 +1,297 @@
+#include "shell/Shell.h"
+#include "nsopenglshell/NSOpenGLTarget.h"
+
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "glgraphics/GLGraphics.h"
+#include "shell/ShellBatteryInfo.h"
+#include "shell/ShellKeyCodes.h"
+#include "shell/ShellThreads.h"
+#include "shell/Target.h"
+
+#include <OpenGL/gl.h>
+
+#define msleep(ms) usleep((ms) * 1000)
+
+static unsigned int timer1ID = UINT_MAX, timer2ID = UINT_MAX;
+static bool deltaMode;
+
+void NSOpenGLTarget_configure(int argc, const char ** argv, struct NSOpenGLShellConfiguration * configuration) {
+	int argIndex;
+	char workingDir[PATH_MAX];
+	
+	printf("NSOpenGLTarget_configure(%d", argc);
+	for (argIndex = 0; argIndex < argc; argIndex++) {
+		printf(", \"%s\"", argv[argIndex]);
+	}
+	printf(", %p)\n", configuration);
+	
+	printf("configuration->windowX: %d\n", configuration->windowX);
+	printf("configuration->windowY: %d\n", configuration->windowY);
+	printf("configuration->windowWidth: %d\n", configuration->windowWidth);
+	printf("configuration->windowHeight: %d\n", configuration->windowHeight);
+	printf("configuration->windowTitle: %s\n", configuration->windowTitle);
+	printf("configuration->displayMode.doubleBuffer: %s\n", configuration->displayMode.doubleBuffer ? "true" : "false");
+	printf("configuration->displayMode.depthBuffer: %s\n", configuration->displayMode.depthBuffer ? "true" : "false");
+	printf("configuration->displayMode.depthBits: %u\n", configuration->displayMode.depthBits);
+	printf("configuration->displayMode.stencilBuffer: %s\n", configuration->displayMode.stencilBuffer ? "true" : "false");
+	printf("configuration->displayMode.stencilBits: %u\n", configuration->displayMode.stencilBits);
+	printf("configuration->displayMode.accumBuffer: %s\n", configuration->displayMode.accumBuffer ? "true" : "false");
+	printf("configuration->displayMode.accumBits: %u\n", configuration->displayMode.accumBits);
+	printf("configuration->displayMode.multisample: %s\n", configuration->displayMode.multisample ? "true" : "false");
+	printf("configuration->displayMode.sampleBuffers: %u\n", configuration->displayMode.sampleBuffers);
+	printf("configuration->displayMode.samples: %u\n", configuration->displayMode.samples);
+	
+	configuration->windowTitle = "NSOpenGLShell Test Harness";
+	printf("configuration->windowTitle = \"%s\"\n", configuration->windowTitle);
+	
+	printf("getcwd(): %s\n", getcwd(workingDir, PATH_MAX));
+}
+
+void Target_init() {
+	printf("Target_init()\n");
+	printf("GLGraphics_getOpenGLAPIVersion(): %d\n", GLGraphics_getOpenGLAPIVersion());
+	Shell_mainLoop();
+}
+
+bool Target_draw() {
+	printf("Target_draw()\n");
+	glClearColor(0.0f, 0.25f, 0.5f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	return true;
+}
+
+static void timerCallback(unsigned int timerID, void * context) {
+	printf("timerCallback(timerID, \"%s\")\n", (char *) context);
+	if (timerID == timer2ID) {
+		timer2ID = UINT_MAX;
+	}
+}
+
+static int threadFunc1(void * context) {
+	printf("Secondary thread 1 %p begin\n", Shell_getCurrentThread());
+	Shell_lockMutex(context);
+	msleep(1000);
+	Shell_unlockMutex(context);
+	printf("Secondary thread 1 %p end\n", Shell_getCurrentThread());
+	return 0;
+}
+
+static int threadFunc2(void * context) {
+	printf("Secondary thread 2 %p begin\n", Shell_getCurrentThread());
+	msleep(1000);
+	Shell_postSemaphore(context);
+	printf("Secondary thread 2 %p end\n", Shell_getCurrentThread());
+	return 0;
+}
+
+static int threadFunc3(void * context) {
+	printf("Secondary thread 3 %p begin\n", Shell_getCurrentThread());
+	msleep(1000);
+	printf("Secondary thread 3 %p exit\n", Shell_getCurrentThread());
+	Shell_exitThread(2);
+	printf("Secondary thread 3 %p end (bad!)\n", Shell_getCurrentThread());
+	return 0;
+}
+
+void Target_keyDown(unsigned int charCode, unsigned int keyCode, unsigned int modifierFlags) {
+	printf("Target_keyDown(%u, %u, 0x%X)\n", charCode, keyCode, modifierFlags);
+	if (keyCode == KEYBOARD_Q) {
+		ShellThread thread;
+		ShellMutex mutex;
+		ShellSemaphore semaphore;
+		bool success;
+		int status;
+		
+		printf("Current thread: %p\n\n", Shell_getCurrentThread());
+		
+		mutex = Shell_createMutex();
+		printf("Created mutex %p\n", mutex);
+		success = Shell_tryLockMutex(mutex);
+		printf("Lock acquired: %s (expected true)\n", success ? "true" : "false");
+		success = Shell_tryLockMutex(mutex);
+		printf("Lock acquired: %s (expected true)\n\n", success ? "true" : "false");
+		Shell_unlockMutex(mutex);
+		Shell_unlockMutex(mutex);
+		
+		thread = Shell_createThread(threadFunc1, mutex);
+		Shell_detachThread(thread);
+		printf("Created thread %p at %f\n", thread, Shell_getCurrentTime());
+		msleep(500);
+		Shell_lockMutex(mutex);
+		printf("Acquired lock at %f\n\n", Shell_getCurrentTime());
+		Shell_unlockMutex(mutex);
+		
+		semaphore = Shell_createSemaphore(0);
+		printf("Try wait semaphore: %s (expected false)\n", Shell_tryWaitSemaphore(semaphore) ? "true" : "false");
+		thread = Shell_createThread(threadFunc2, semaphore);
+		Shell_detachThread(thread);
+		printf("Created thread %p at %f\n", thread, Shell_getCurrentTime());
+		printf("Try wait semaphore: %s (expected false)\n", Shell_tryWaitSemaphore(semaphore) ? "true" : "false");
+		Shell_waitSemaphore(semaphore);
+		printf("Acquired semaphore at %f\n\n", Shell_getCurrentTime());
+		
+		thread = Shell_createThread(threadFunc3, mutex);
+		printf("Created thread %p at %f\n", thread, Shell_getCurrentTime());
+		status = Shell_joinThread(thread);
+		printf("Joined thread at %f, with status %d\n\n", Shell_getCurrentTime(), status);
+		
+		Shell_disposeMutex(mutex);
+		Shell_disposeSemaphore(semaphore);
+		
+	} else if (keyCode == KEYBOARD_T) {
+		printf("Shell_getCurrentTime(): %f\n", Shell_getCurrentTime());
+		
+	} else if (keyCode == KEYBOARD_R) {
+		printf("Shell_getResourcePath(): %s\n", Shell_getResourcePath());
+		
+	} else if (keyCode == KEYBOARD_Y) {
+		printf("Shell_getSupportPath(NULL): %s\n", Shell_getSupportPath(NULL));
+		printf("Shell_getSupportPath(\"nsopenglshell\"): %s\n", Shell_getSupportPath("nsopenglshell"));
+		
+	} else if (keyCode == KEYBOARD_D) {
+		Shell_redisplay();
+		
+	} else if (keyCode == KEYBOARD_F) {
+		printf("Shell_setFullScreen(true): %s\n", Shell_setFullScreen(true) ? "true" : "false");
+		
+	} else if (keyCode == KEYBOARD_W) {
+		printf("Shell_setFullScreen(false): %s\n", Shell_setFullScreen(false) ? "true" : "false");
+		
+	} else if (keyCode == KEYBOARD_G) {
+		printf("Shell_isFullScreen(): %s\n", Shell_isFullScreen() ? "true" : "false");
+		
+	} else if (keyCode == KEYBOARD_B) {
+		printf("Shell_getBatteryState(): %d\n", Shell_getBatteryState());
+		printf("Shell_getBatteryLevel(): %f\n", Shell_getBatteryLevel());
+		
+	} else if (keyCode == KEYBOARD_X) {
+		unsigned int width = 0, height = 0;
+		
+		Shell_getMainScreenSize(&width, &height);
+		printf("Shell_getMainScreenSize(%u, %u)\n", width, height);
+		
+	} else if (keyCode == KEYBOARD_COMMA) {
+		if (timer1ID == UINT_MAX) {
+			timer1ID = Shell_setTimer(1.0, true, timerCallback, "Timer 1 context");
+			printf("Shell_setTimer(1.0, true, %p, \"Timer 1 context\"): %u\n", timerCallback, timer1ID);
+		} else {
+			printf("Shell_cancelTimer(%u)\n", timer1ID);
+			Shell_cancelTimer(timer1ID);
+			timer1ID = UINT_MAX;
+		}
+		
+	} else if (keyCode == KEYBOARD_PERIOD) {
+		if (timer2ID == UINT_MAX) {
+			timer2ID = Shell_setTimer(2.0, false, timerCallback, "Timer 2 context");
+			printf("Shell_setTimer(2.0, false, %p, \"Timer 2 context\"): %u\n", timerCallback, timer2ID);
+		} else {
+			printf("Shell_cancelTimer(%u)\n", timer2ID);
+			Shell_cancelTimer(timer2ID);
+			timer2ID = UINT_MAX;
+		}
+		
+	} else if (keyCode == KEYBOARD_SEMICOLON) {
+		deltaMode = !deltaMode;
+		Shell_setMouseDeltaMode(deltaMode);
+		printf("Shell_setMouseDeltaMode(%s)\n", deltaMode ? "true" : "false");
+		
+	} else if (keyCode == KEYBOARD_H) {
+		Shell_setCursorVisible(false);
+		
+	} else if (keyCode == KEYBOARD_S) {
+		Shell_setCursorVisible(true);
+		
+	} else if (keyCode == KEYBOARD_M) {
+		Shell_hideCursorUntilMouseMoves();
+		
+	} else if (keyCode == KEYBOARD_0 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(ShellCursor_arrow);
+		
+	} else if (keyCode == KEYBOARD_1 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(ShellCursor_iBeam);
+		
+	} else if (keyCode == KEYBOARD_2 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(ShellCursor_crosshair);
+		
+	} else if (keyCode == KEYBOARD_3 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_closedHand);
+		
+	} else if (keyCode == KEYBOARD_4 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_openHand);
+		
+	} else if (keyCode == KEYBOARD_5 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(ShellCursor_hand);
+		
+	} else if (keyCode == KEYBOARD_6 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_resizeLeft);
+		
+	} else if (keyCode == KEYBOARD_7 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_resizeRight);
+		
+	} else if (keyCode == KEYBOARD_8 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_resizeLeftRight);
+		
+	} else if (keyCode == KEYBOARD_9 && !(modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_resizeUp);
+		
+	} else if (keyCode == KEYBOARD_1 && (modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_resizeDown);
+		
+	} else if (keyCode == KEYBOARD_2 && (modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_resizeUpDown);
+		
+	} else if (keyCode == KEYBOARD_3 && (modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_disappearingItem);
+		
+	} else if (keyCode == KEYBOARD_4 && (modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_contextualMenu);
+		
+	} else if (keyCode == KEYBOARD_5 && (modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_dragCopy);
+		
+	} else if (keyCode == KEYBOARD_6 && (modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_dragLink);
+		
+	} else if (keyCode == KEYBOARD_7 && (modifierFlags & MODIFIER_SHIFT_BIT)) {
+		Shell_setCursor(NSOpenGLShellCursor_operationNotAllowed);
+	}
+}
+
+void Target_keyUp(unsigned int keyCode, unsigned int modifierFlags) {
+	printf("Target_keyUp(%u, 0x%X)\n", keyCode, modifierFlags);
+}
+
+void Target_keyModifiersChanged(unsigned int modifierFlags) {
+	printf("Target_keyModifiersChanged(0x%X)\n", modifierFlags);
+}
+
+void Target_mouseDown(unsigned int buttonNumber, float x, float y) {
+	printf("Target_mouseDown(%d, %f, %f)\n", buttonNumber, x, y);
+}
+
+void Target_mouseUp(unsigned int buttonNumber, float x, float y) {
+	printf("Target_mouseUp(%d, %f, %f)\n", buttonNumber, x, y);
+}
+
+void Target_mouseMoved(float x, float y) {
+	printf("Target_mouseMoved(%f, %f)\n", x, y);
+}
+
+void Target_mouseDragged(unsigned int buttonMask, float x, float y) {
+	printf("Target_mouseDragged(0x%X, %f, %f)\n", buttonMask, x, y);
+}
+
+void Target_resized(unsigned int newWidth, unsigned int newHeight) {
+	printf("Target_resized(%d, %d)\n", newWidth, newHeight);
+	glViewport(0, 0, newWidth, newHeight);
+}
+
+void Target_backgrounded() {
+	printf("Target_backgrounded()\n");
+}
+
+void Target_foregrounded() {
+	printf("Target_foregrounded()\n");
+}
