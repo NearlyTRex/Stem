dccfcb7cdca782075f37a554ef4fcd62143aaff8
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2014-06-29 21:07:14 +0000

    Tag resourcemanager 1.0.4
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@1942 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ resourcemanager/tags/1.0.4/ReadMe.txt
@@ -0,0 +1,55 @@
+= Overview =
+
+ResourceManager is a StemObject subclass that provides facilities for managing the loading, retrieval, and unloading of resources. The definition of "resource" is completely up to the client; ResourceManager represents resources as void * and provides hooks for the loading and unloading of them to be implemented by the calling code.
+
+= Basic Usage =
+
+After instantiating a ResourceManager, you must add one or more type handlers in order to be able to do anything meaningful with it. A type handler includes a type name string, an optional callback that loads a resource of that type, an optional callback that unloads a resource of that type, and a context pointer. The context pointer is passed to both the loadFunction and the unloadFunction when they are called.
+
+Once your type handlers are added, there are two ways to make resources available for retrieval from a ResourceManager: you can call addResource() to add one directly, or you can call referenceResource() to allow it to be loaded by your provided loadFunction. If you haven't provided a loadFunction, you must use addResource() for every resource you will subsequently reference.
+
+Once a resource has been loaded, ResourceManager keeps it in its cache along with a reference count. A resource starts with a reference count of 1 when loaded via addResource() or referenceResource(). Subsequent calls to referenceResource() increment the reference count, and calls to releaseResource() decrement it. If the reference count drops to 0 as a result of calling releaseResource(), your unload function (if you've provided one) will be called.
+
+= Example =
+
+static ResourceManager * resourceManager;
+
+static void * loadPNG(const char * resourceName, void * context);
+static void unloadPNG(void * resource, void * context);
+struct PNGSettings {
+	int pixelFormat;
+	bool flipVertical;
+} defaultPNGSettings = {PNG_PIXEL_FORMAT_AUTOMATIC, false};
+
+void Target_init() {
+	char * string;
+	
+	resourceManager = ResourceManager_create();
+	resourceManager->addTypeHandler(resourceManager, "string", NULL, NULL, NULL);
+	resourceManager->addTypeHandler(resourceManager, "png", loadPNG, unloadPNG, &defaultPNGSettings);
+	resourceManager->addResource(resourceManager, "string", "hello", "Hello, world!");
+	
+	string = resourceManager->referenceResource(resourceManager, "string", "hello");
+	printf("%s\n", string);
+	resourceManager->releaseResource(resourceManager, "string", "hello");
+	
+	Shell_mainLoop();
+}
+
+bool Target_draw() {
+	BitmapImage * image;
+	
+	image = resourceManager->referenceResource(resourceManager, "png", "myimage.png");
+	// Draw image
+	resourceManager->releaseResource(resourceManager, "png", "myimage.png");
+	return true;
+}
+
+static void * loadPNG(const char * resourceName, void * context) {
+	struct PNGSettings * settings = context;
+	return PNGImageIO_loadPNGFile(resourcePath(resourceName), settings.pixelFormat, settings.flipVertical);
+}
+
+static void unloadPNG(void * resource, void * context) {
+	((BitmapImage *) resource)->dispose(resource);
+}
---
+++ resourcemanager/tags/1.0.4/source/resourcemanager/ResourceManager.h
@@ -0,0 +1,91 @@
+/*
+  Copyright (c) 2013 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#ifndef __RESOURCE_MANAGER_H__
+#define __RESOURCE_MANAGER_H__
+
+typedef struct ResourceManager ResourceManager;
+
+#include "stemobject/StemObject.h"
+#include <stdlib.h>
+
+struct ResourceManager_typeHandler {
+	char * typeName;
+	void * (* loadFunction)(const char * resourceName, void * context);
+	void (* unloadFunction)(void * resource, void * context);
+	void * context;
+};
+
+struct ResourceManager_resource {
+	const char * typeName;
+	char * resourceName;
+	void * resource;
+	unsigned int referenceCount;
+};
+
+#define ResourceManager_structContents(self_type) \
+	StemObject_structContents(self_type) \
+	\
+	size_t typeHandlerCount; \
+	struct ResourceManager_typeHandler * typeHandlers; \
+	size_t resourceCount; \
+	struct ResourceManager_resource * resources; \
+	\
+	void (* addTypeHandler)(self_type * self, \
+	                        const char * typeName, \
+	                        void * (* loadFunction)(const char * resourceName, void * context), \
+	                        void (* unloadFunction)(void * resource, void * context), \
+	                        void * context); \
+	void (* addResource)(self_type * self, \
+	                     const char * typeName, \
+	                     const char * resourceName, \
+	                     void * resource); \
+	void * (* referenceResource)(self_type * self, \
+	                             const char * typeName, \
+	                             const char * resourceName); \
+	void (* releaseResource)(self_type * self, \
+	                         const char * typeName, \
+	                         const char * resourceName);
+
+stemobject_struct_definition(ResourceManager)
+
+ResourceManager * ResourceManager_create();
+bool ResourceManager_init(ResourceManager * self);
+void ResourceManager_dispose(ResourceManager * self);
+
+void ResourceManager_addTypeHandler(ResourceManager * self,
+                                    const char * typeName,
+                                    void * (* loadFunction)(const char * resourceName, void * context),
+                                    void (* unloadFunction)(void * resource, void * context),
+                                    void * context);
+void ResourceManager_addResource(ResourceManager * self,
+                                 const char * typeName,
+                                 const char * resourceName,
+                                 void * resource);
+void * ResourceManager_referenceResource(ResourceManager * self,
+                                         const char * typeName,
+                                         const char * resourceName);
+void ResourceManager_releaseResource(ResourceManager * self,
+                                     const char * typeName,
+                                     const char * resourceName);
+
+#endif
