1bd478e9b0bbbb47dd02825774562ba7c61f1384
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2014-06-30 01:18:15 +0000

    Tag preferences 1.1.4
    git-svn-id: http://ludobloom.com/svn/StemLibProjects@1969 d291a34c-4c4d-0410-8556-a73de5c6a500

---
+++ preferences/tags/1.1.4/ReadMe.txt
@@ -0,0 +1,34 @@
+Preferences provides a simple interface for persistent key-value storage in a location appropriate for the user's platform.
+
+After creating a Preferences object, the schema of your preferences keys and values must be set up by a series of function calls. addInteger, addFloat, addBoolean, addString, and addData must be called for each key/value the Preferences object is to handle.
+
+Once keys have been registered, you can call load() to read all of the specified values from disk; getInteger(), getFloat(), getBoolean(), getString(), and getData() to access them; setInteger(), setFloat(), setBoolean(), setString(), and setData() to modify them; and save() to write them back to disk. You can also use loadDefaultValue() to reset a single value to its default, or loadDefaultValues() to set all values to their defaults.
+
+If you want to save a file separate from the one maintained by Preferences itself, you can use the Preferences_getFilePath() function to retrieve an appropriate file path for it.
+
+Example:
+
+	Preferences * preferences;
+	
+	preferences = Preferences_create("example");
+	
+	preferences->addInteger(preferences, "integer", 1);
+	preferences->addFloat(preferences, "float", 4.0f);
+	preferences->addBoolean(preferences, "boolean", true);
+	preferences->addString(preferences, "string", "Hello, world!");
+	
+	preferences->load(preferences);
+	
+	printf("integer: %d\n", preferences->getInteger(preferences, "integer"));
+	printf("float: %f\n", preferences->getFloat(preferences, "float"));
+	printf("boolean: %s\n", preferences->getBoolean(preferences, "boolean") ? "true" : "false");
+	printf("string: %s\n", preferences->getString(preferences, "string"));
+	
+	preferences->setInteger(preferences, "integer", -50);
+	preferences->setFloat(preferences, "float", -1.0f);
+	preferences->setBoolean(preferences, "boolean", false);
+	preferences->setString(preferences, "string", "foo");
+	
+	preferences->save(preferences);
+	
+	preferences->dispose(preferences);
---
+++ preferences/tags/1.1.4/source/preferences/Preferences.h
@@ -0,0 +1,153 @@
+/*
+  Copyright (c) 2013 Alex Diener
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+  Alex Diener adiener@sacredsoftware.net
+*/
+
+#ifndef __Preferences_H__
+#define __Preferences_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct Preferences Preferences;
+
+// EventData is a struct PreferencesEvent *. If the event handler modifies the event's value,
+// the modified value will be saved instead of the original value. If the value is of a string
+// or data type and the event handler reassigns the string/bytes pointer, the assigned pointer
+// must remain valid after the event handler completes, but it is the handler's responsibility
+// to ensure that the pointer is subsequently freed.
+#define PREFERENCES_EVENT_VALUE_CHANGED "PREFERENCES_EVENT_VALUE_CHANGED"
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+enum PreferencesType {
+	PREFERENCES_TYPE_INTEGER,
+	PREFERENCES_TYPE_FLOAT,
+	PREFERENCES_TYPE_BOOLEAN,
+	PREFERENCES_TYPE_STRING,
+	PREFERENCES_TYPE_DATA
+};
+
+union PreferencesValue {
+	int integer;
+	float number;
+	bool boolean;
+	char * string;
+	struct {
+		void * bytes;
+		size_t length;
+	} data;
+};
+
+union PreferencesValueConst {
+	int integer;
+	float number;
+	bool boolean;
+	const char * string;
+	struct {
+		const void * bytes;
+		size_t length;
+	} data;
+};
+
+struct PreferencesEvent {
+	const char * name;
+	enum PreferencesType type;
+	union PreferencesValueConst value;
+	union PreferencesValueConst previousValue;
+};
+
+#include "stemobject/StemObject.h"
+#include "utilities/EventDispatcher.h"
+
+struct Preferences_valueRecord {
+	char * name;
+	enum PreferencesType type;
+	union PreferencesValue value;
+	union PreferencesValue defaultValue;
+};
+
+#define Preferences_structContents(self_type) \
+	StemObject_structContents(self_type) \
+	\
+	const char * identifier; \
+	size_t valueCount; \
+	struct Preferences_valueRecord * values; \
+	EventDispatcher * eventDispatcher; \
+	\
+	void (* addInteger)(self_type * self, const char * name, int defaultValue); \
+	void (* addFloat)(self_type * self, const char * name, float defaultValue); \
+	void (* addBoolean)(self_type * self, const char * name, bool defaultValue); \
+	void (* addString)(self_type * self, const char * name, const char * defaultValue); \
+	void (* addData)(self_type * self, const char * name, const void * defaultValue, size_t defaultLength); \
+	\
+	int (* getInteger)(self_type * self, const char * name); \
+	float (* getFloat)(self_type * self, const char * name); \
+	bool (* getBoolean)(self_type * self, const char * name); \
+	const char * (* getString)(self_type * self, const char * name); \
+	void * (* getData)(self_type * self, const char * name, size_t * outLength); \
+	\
+	void (* setInteger)(self_type * self, const char * name, int value); \
+	void (* setFloat)(self_type * self, const char * name, float value); \
+	void (* setBoolean)(self_type * self, const char * name, bool value); \
+	void (* setString)(self_type * self, const char * name, const char * value); \
+	void (* setData)(self_type * self, const char * name, const void * value, size_t length); \
+	\
+	void (* load)(self_type * self); \
+	void (* save)(self_type * self); \
+	void (* loadDefaultValues)(self_type * self); \
+	void (* loadDefaultValue)(self_type * self, const char * name);
+
+stemobject_struct_definition(Preferences)
+
+void Preferences_getFilePath(const char * fileName, char * outPath);
+
+Preferences * Preferences_create(const char * identifier);
+bool Preferences_init(Preferences * self, const char * identifier);
+void Preferences_dispose(Preferences * self);
+
+void Preferences_addInteger(Preferences * self, const char * name, int defaultValue);
+void Preferences_addFloat(Preferences * self, const char * name, float defaultValue);
+void Preferences_addBoolean(Preferences * self, const char * name, bool defaultValue);
+void Preferences_addString(Preferences * self, const char * name, const char * defaultValue);
+void Preferences_addData(Preferences * self, const char * name, const void * defaultValue, size_t defaultLength);
+
+int Preferences_getInteger(Preferences * self, const char * name);
+float Preferences_getFloat(Preferences * self, const char * name);
+bool Preferences_getBoolean(Preferences * self, const char * name);
+const char * Preferences_getString(Preferences * self, const char * name);
+void * Preferences_getData(Preferences * self, const char * name, size_t * outLength);
+
+void Preferences_setInteger(Preferences * self, const char * name, int value);
+void Preferences_setFloat(Preferences * self, const char * name, float value);
+void Preferences_setBoolean(Preferences * self, const char * name, bool value);
+void Preferences_setString(Preferences * self, const char * name, const char * value);
+void Preferences_setData(Preferences * self, const char * name, const void * value, size_t length);
+
+void Preferences_load(Preferences * self);
+void Preferences_save(Preferences * self);
+void Preferences_loadDefaultValues(Preferences * self);
+void Preferences_loadDefaultValue(Preferences * self, const char * name);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
